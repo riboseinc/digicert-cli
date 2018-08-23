77e35e0b72985e8dfd02ad2e7fea1206b51a1851
Abu Nashir <abunashir@gmail.com>
2018-08-23 16:54:34 +0200

    Bump the gem version to `0.4.2`
    * Fix table width related rendering issue

--- lib/digicert/cli/version.rb
+++ lib/digicert/cli/version.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (c) 2017 Ribose Inc.
+# Copyright (c) 2018 Ribose Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,6 +22,6 @@
 
 module Digicert
   module CLI
-    VERSION = "0.4.1".freeze
+    VERSION = "0.4.2".freeze
   end
 end
