b8fcef4a724db82e0e2f1eb28ed8d1da53f40c65
Abu Nashir <abunashir@gmail.com>
2018-08-23 16:38:42 +0200

    Fix table related rendering issue
    Currently, we have a limit for the table width to 100, but as
    mentioned in #58, it was throwing an error. This commit changes
    this limit to be flexible, so this should fix the ongoing issue

--- lib/digicert/cli/util.rb
+++ lib/digicert/cli/util.rb
@@ -3,11 +3,9 @@ require "terminal-table"
 module Digicert
   module CLI
     module Util
-      def self.make_it_pretty(headings:, rows:, table_wdith: 100)
+      def self.make_it_pretty(headings:, rows:)
         Terminal::Table.new do |table|
           table.headings = headings
-          table.style = { width: table_wdith }
-
           table.rows = rows
         end
       end
